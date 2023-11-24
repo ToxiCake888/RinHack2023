94bcc0d3dca972b2e494d2a4353aefda04607dfb
Alexey Romanenkov <143901241+ToxiCake888@users.noreply.github.com>
2023-11-24 13:30:39 +0300

    Add files via upload

---
+++ hackaton/testapp/testapp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Text.Unicode;
+
+Console.WriteLine("Hello, World!");
+
+
+class sus
+{
+    static void Main()
+    {
+        string mypath ="сотрудники.txt";
+        string text = "";
+        using (StreamReader sr = new(mypath, System.Text.Encoding.Default))
+        {
+            text = sr.ReadToEnd();
+        }
+
+        Console.WriteLine(text);
+    }
+}
